de41a193ce97ed21723a3eb382ff6c801f87cdc7
Julio Leon <juliopena143@icloud.com>
2025-03-27 13:59:07 -0600

    imports finally working

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 # Imports
+# Data
 import pandas as pd
 import numpy as np
-
+# Machine Learning
 from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+# Visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
