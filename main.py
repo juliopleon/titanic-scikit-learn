459141d4a0c5d6be4fe9505f9fe54c53ae7c595e
Julio Leon <juliopena143@icloud.com>
2025-05-20 00:31:01 -0600

    create features / target variables

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ def preprocess_data(df):
     df["Embarked"].fillna("S", inplace=True)
     df.drop(columns=["Embarked"], inplace=True)
 
+    fill_missing_ages(df)
+
     # Convert gender
     df["Sex"] = df["Sex"].map({'male':1,'female':0})
 
@@ -42,3 +44,17 @@ def fill_missing_ages(df):
             age_fill_map[pclass] = df[df["Pclass"] == pclass]["Age"].median()
 
     df["Age"] = df.apply(lambda row: age_fill_map[row["Pclass"]] if pd.isnull(row["Age"]) else row["Age"],axis=1)
+
+data = preprocess_data(data)
+
+# Create Features / Target Variables (Make Flashcards)
+X = data.drop(columns={"Survived"})
+y = data["Survived"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+
+# ML Processing
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
